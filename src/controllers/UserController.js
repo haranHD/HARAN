083d09c78cd1739e08af119672ecd41b583f1657
haranHD <haricode04@gmail.com>
2026-05-30 18:40:07 +0530

    userController updated for service layer

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,20 +1,40 @@
-const User = require('../models/User');
+// const User = require('../models/User');
+const userService = require('../services/userService');
 
 //ADD USER
 exports.addUser = async (req, res) => {
-    const user = await User.create(req.body);
+    const user = await userService.addUser(req.body);
     res.status(201).json(user);
 
 }
 
 //GET ALL USER
 exports.getUser = async (req, res) => {
-    const user = await User.find();
+    const user = await userService.findUser();
     res.json(user);
 }
 
 //GET BY ID
 exports.getById = async (req, res) => {
-    const user = await User.findById(req.params.id);
-    res.status(200).json(user);
+    const user = await userService.findById(req.params.id);
+    if (!user) {
+        res.status(404).json({
+            message: "User Not Found!"
+        })
+    } else
+        res.status(200).json(user);
+}
+
+//Delete By ID
+exports.delById = async (req, res) => {
+    const user = await userService.delById(req.params.id);
+    if (!user) {
+        res.status(404).json({
+            message: "User Not Found!"
+        });
+    }
+    else
+        res.status(200).json({
+            message: "User Deleted!"
+        });
 }
