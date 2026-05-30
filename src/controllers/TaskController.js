18959e7d4c90d78e00a8ae66b5af3a408ec09309
haranHD <haricode04@gmail.com>
2026-05-30 22:24:47 +0530

    taskController.js

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,21 +1,22 @@
-const Task = require('../models/Task');
+// const Task = require('../models/Task');
+const taskService = require("../services/taskService");
 
 
 //Add Task:
 exports.addTask = async (req, res) => {
-    const task = await Task.create(req.body);
+    const task = await taskService.addTask(req.body);
     res.status(201).json(task);
 }
 
 //Get Task:
 exports.getTask = async (req, res) => {
-    const task = await Task.find();
+    const task = await taskService.getTask();
     res.status(200).json(task);
 }
 
 //Delete Task:
 exports.delTask = async (req, res) => {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await taskService.delTask(req.params.id);
     if (!task) {
         res.status(404).json({
             message: "Task Not Found!"
