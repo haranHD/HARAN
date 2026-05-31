9550902788b9ecacc1cb31aabc1a3eb79a081410
haranHD <haricode04@gmail.com>
2026-05-31 20:22:51 +0530

    DB

--- src/DB/db.js
+++ src/DB/db.js
@@ -5,5 +5,5 @@ mongoose.connect("mongodb://localhost:27017/HARAN")
         console.log("DB is connected");
     })
     .catch((err) => {
-        console.log("ERROR : ", err).message;
+        console.log("ERROR : ", err.message);
     });
