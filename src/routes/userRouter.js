9ea1b2e92fbdc3ebbfc142ff5599fb792dbb7081
haranHD <haricode04@gmail.com>
2026-06-01 16:14:16 +0530

    router updated

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,12 +2,13 @@ const User = require('../models/User');
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/UserController');
+const auth = require('../middleware/authMiddleware');
 
 //USER 
 router.post('/userLogin', userController.login);
 router.post('/addUser', userController.addUser);
-router.get('/getUser', userController.getUser);
-router.get('/getbyid/:id', userController.getById);
-router.delete('/delById/:id', userController.delById);
+router.get('/getUser', auth.authenticate, userController.getUser);
+router.get('/getbyid/:id', auth.authenticate, userController.getById);
+router.delete('/delById/:id', auth.authenticate, userController.delById);
 
 module.exports = router;
