4dd674b7d0f0284dcca9b29d3173d735c9cce4b2
haranHD <haricode04@gmail.com>
2026-06-03 12:36:47 +0530

    routes modified based on user and admin roles

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -2,10 +2,13 @@ const taskController = require('../controllers/TaskController');
 const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
+const auth = require('../middleware/authMiddleware')
+const role = require('../middleware/roleMiddleware');
 
 //Task
-router.post('/addTask', taskController.addTask);
-router.get('/getTask', taskController.getTask);
+router.get('/allTasks', auth.authenticate, role.authorize('ADMIN'), taskController.getAllTask);
+router.post('/addTask', auth.authenticate, taskController.addTask);
+router.get('/getTask', auth.authenticate, taskController.getTask);
 router.patch('/updateTask/:id', taskController.updateTask);
 router.delete('/delTask/:id', taskController.delTask);
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/UserController');
 const auth = require('../middleware/authMiddleware');
+const role = require('../middleware/roleMiddleware');
 
 //USER 
-router.post('/userLogin', userController.login);
+router.post('/login', userController.login);
 router.post('/addUser', userController.addUser);
-router.get('/getUser', auth.authenticate, userController.getUser);
+router.get('/getUser', auth.authenticate, role.authorize('ADMIN'), userController.getUser);//GET ALL USER ONLY BY ADMIN
 router.get('/getbyid/:id', auth.authenticate, userController.getById);
-router.delete('/delById/:id', auth.authenticate, userController.delById);
+router.delete('/delById/:id', auth.authenticate, role.authorize('ADMIN'), userController.delById);//DELETE A USER ONLY BY ADMIN
+router.get('/profile', auth.authenticate, userController.profile);// PROFILE 
 
 module.exports = router;
