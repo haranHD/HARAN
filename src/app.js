a42a5e137e6ddf5e8731e9f216153464320031e2
haranHD <haricode04@gmail.com>
2026-06-03 12:38:10 +0530

    app.js modified for cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 require('dotenv').config();//.env config
 const express = require('express');
 const app = express();
+const cors = require('cors');// to handle cors (cross origin resource sharing) bypass the brower's default security
 
 const UserRouter = require('./routes/userRouter');
 const TaskRouter = require('./routes/taskRouter');
 
 require('./config/db');// DB config
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 //Router Mapping
 app.use('/api/users', UserRouter); //USER
